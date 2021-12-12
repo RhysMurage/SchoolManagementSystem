a0c36c20ce1edf8e0a80a3e368f222ac764382cc
RhysMurage <rhysmwangi96@gmail.com>
2021-12-12 22:33:12 +0300

    commented out incomplete code for views

---
+++ schoolsystem/students/api/serializers.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db.models import fields
+
+from students.models import *
+
+from rest_framework import serializers
+
+# class StudentListSerializer(serializers.ModelSerializer):
